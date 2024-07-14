1284d71f3f653aff5429738934223c46aed05eca
kmh0831 <kmhyuk1018@gmail.com>
2024-07-14 15:49:34 +0900

    S3

--- s3.tf
+++ s3.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-# S3 버킷 생성
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = "s3-website-bucket-kmhyuk1018-1"  # 버킷 이름을 원하는 이름으로 변경하세요.
+resource "aws_s3_bucket" "learn-terraform-mybucket" {
+  bucket = "s3-kmhyuk0831-01"
+
   tags = {
     environment = "devel"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket = aws_s3_bucket.website_bucket.id
+resource "aws_s3_bucket_public_access_block" "public-access" {
+  bucket = aws_s3_bucket.learn-terraform-mybucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -19,64 +19,31 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
-  
-  depends_on = [
-    aws_s3_bucket_public_access_block.public_access
-  ]
-  
-  policy = <<EOF
-  {
-      "Version": "2012-10-17", 
-      "Statement": [ 
-        { 
-          "Sid": "Statement1", 
-          "Effect": "Allow", 
-          "Principal": "*", 
-          "Action": "s3:GetObject", 
-          "Resource": "${aws_s3_bucket.website_bucket.arn}/*" 
-        }
-      ]
-    }
-  EOF
+resource "aws_s3_object" "learn-terraform-sample-txt" {
+  bucket = aws_s3_bucket.learn-terraform-mybucket.id
+  key    = "sample.txt"
+  source = "sample.txt"
 }
 
+resource "aws_s3_bucket_policy" "bucket-policy" {
+  bucket = aws_s3_bucket.learn-terraform-mybucket.id
 
-# resource "aws_s3_object" "app-code-files" {
-#     for_each = fileset("C:/Users/ghrfhdg/Desktop/WEB/", "**")
-#         # fileset : 지정 경로에 대한 파일 이름 집합 열거
-#             # ** : fileset 함수에서 재귀 검색 패턴 적용
-#             # * : 재귀 검색 없이 단일 검색 패턴으로 적용 시 폴더는 업로드 되지 않음
-#         # for_each : fileset 함수에서 반환된 문서에 대한 반복을 수행하는 인수
+  depends_on = [
+    aws_s3_bucket_public_access_block.public-access
+  ]
 
-#     bucket = aws_s3_bucket.website_bucket.id
-#         # 업로드 할 Bucket 지정
-#             # .id 를 통해 생성하기로 한 버킷에 대한 자동 인식
-#     key = each.value
-#         # key = bucket에 업로드 시, 지정되는 객체의 이름
-#             # each.value : 업로드 할 각 파일(폴더)명을 그대로 식별하도록 지정
-#     source = "C:/Users/ghrfhdg/Desktop/WEB/${each.value}"
-#         # 업로드 대상에 대한 출처(경로) 지정 및 내부 파일(폴더)들을 각각 지정
-# }
-
-# resource "aws_s3_object" "index_html" {
-#   bucket = aws_s3_bucket.website_bucket.id
-#   key    = "index.html"
-#   source = "index.html"  # 로컬 파일 경로
-#   acl    = "public-read"
-# }
-
-# resource "aws_s3_object" "product1_html" {
-#   bucket = aws_s3_bucket.website_bucket.id
-#   key    = "product1.html"
-#   source = "product1.html"  # 로컬 파일 경로
-#   acl    = "public-read"
-# }
-
-# resource "aws_s3_object" "styles_css" {
-#   bucket = aws_s3_bucket.website_bucket.id
-#   key    = "styles.css"
-#   source = "styles.css"  # 로컬 파일 경로
-#   acl    = "public-read"
-# }
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${aws_s3_bucket.learn-terraform-mybucket.id}/*"]
+    }
+  ]
+}
+POLICY
+}
