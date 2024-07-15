4ee4b343bd16c21e45648bbdddbe9f71fc0355e8
kmh0831 <kmhyuk1018@gmail.com>
2024-07-15 14:53:42 +0900

    Terraform-Infra

--- web-ec2.tf
+++ web-ec2.tf
@@ -34,8 +34,8 @@ resource "aws_iam_role_policy" "example" {
         ],
         Effect = "Allow",
         Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.learn_terraform_mybucket.bucket}/*",
-          "arn:aws:s3:::${aws_s3_bucket.learn_terraform_mybucket.bucket}"
+          "arn:aws:s3:::s3-kmhyuk0831-01",
+          "arn:aws:s3:::s3-kmhyuk0831-01/*"
         ]
       },
     ],
